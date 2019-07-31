2675ef9356f14d589553df7476814258e5d9c564
Long Nguyen <olragon@gmail.com>
2019-07-31 22:55:04 +0700

    Upgrade dev packages and rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,44 @@
-const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-let libraryName = 'BinPacking', plugins = [], outputFile;
+let libraryName = 'BinPacking',
+	plugins = [],
+	outputFile;
 const env = process.env.WEBPACK_ENV;
 
 if (env === 'build') {
-  plugins.push(new UglifyJSPlugin({
-  	sourceMap: true
-  }));
-  outputFile = `[name].min.js`;
+	plugins.push(
+		new UglifyJSPlugin({
+			sourceMap: true
+		})
+	);
+	outputFile = `[name].min.js`;
 } else {
-  outputFile = `[name].js`;
+	outputFile = `[name].js`;
 }
 
 module.exports = {
-	context: __dirname + "/src",
-  entry: {
-    BP2D: './2D',
-    BP3D: './3D',
-    BinPacking: './index'
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: "umd",
-    umdNamedDefine: true    
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  plugins: plugins
-}
+	context: __dirname + '/src',
+	entry: {
+		BP2D: './2D',
+		BP3D: './3D',
+		BinPacking: './index'
+	},
+	output: {
+		path: __dirname + '/dist',
+		filename: outputFile,
+		library: libraryName,
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: plugins,
+	mode: env === 'build' ? 'production' : 'development'
+};
