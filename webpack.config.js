0f4f95cd81a75b2dcd0bb4d36a6548b488e38981
Long Nguyen <olragon@gmail.com>
2021-02-27 17:21:09 +0700

    chore: upgrade webpack, enjoy smaller build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,10 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-let libraryName = 'BinPacking',
-	plugins = [],
-	outputFile;
 const isMinify = !!process.env.BUILD_MINIFY;
-
-if (isMinify) {
-	plugins.push(
-		new UglifyJSPlugin({
-			sourceMap: true
-		})
-	);
-	outputFile = `[name].min.js`;
-} else {
-	outputFile = `[name].js`;
-}
 
 module.exports = {
 	context: __dirname + '/src',
+	optimization: {
+		minimize: isMinify
+	},
 	entry: {
 		BP2D: './2D',
 		BP3D: './3D',
@@ -25,8 +12,8 @@ module.exports = {
 	},
 	output: {
 		path: __dirname + '/dist',
-		filename: outputFile,
-		library: libraryName,
+		filename: isMinify ? '[name].min.js' : '[name].js',
+		library: 'BinPacking',
 		libraryTarget: 'umd',
 		umdNamedDefine: true,
 		globalObject: 'this'
@@ -40,6 +27,5 @@ module.exports = {
 			}
 		]
 	},
-	plugins: plugins,
 	mode: isMinify ? 'production' : 'development'
 };
